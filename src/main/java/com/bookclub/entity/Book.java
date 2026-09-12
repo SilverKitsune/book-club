ac283e1e1e03634dd344a97d68f1babca70bccf9
Allo4ka <zhalla1999@mail.ru>
2026-09-12 18:16:15 +0300

    create entities

---
+++ src/main/java/com/bookclub/entity/Book.java
@@ -0,0 +1,65 @@
+package com.bookclub.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String author;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String annotation;
+
+    @Column
+    private String genre;
+
+    @Column(name = "is_audio")
+    private Boolean isAudio = false;
+
+    @Column
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getAnnotation() { return annotation; }
+    public void setAnnotation(String annotation) { this.annotation = annotation; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public Boolean getIsAudio() { return isAudio; }
+    public void setIsAudio(Boolean isAudio) { this.isAudio = isAudio; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public Meeting getMeeting() { return meeting; }
+    public void setMeeting(Meeting meeting) { this.meeting = meeting; }
+}
---
+++ src/main/java/com/bookclub/entity/User.java
@@ -0,0 +1,45 @@
+package com.bookclub.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
